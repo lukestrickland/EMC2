173136aeef0283118d67b7f0300e2fdef05e5384
Luke Strickland <lukejgstrickland@gmail.com>
2026-05-01 14:54:00 +0800

    Conditional DIC output
    Swaps DIC/BPIC for conditional outputs.

--- R/statistics.R
+++ R/statistics.R
@@ -1,14 +1,26 @@
-#' Information Criteria and Marginal Likelihoods
+#' Information Criteria and Log Marginal Likelihood
 #'
-#' Returns the BPIC/DIC or marginal deviance (-2*marginal likelihood) for a list of samples objects.
+#' Returns the BPIC/DIC and optionally marginal deviance (-2 x log marginal likelihood) for a list of samples objects.
+#'
+#' Computes DIC and BPIC using a deviance based on either (a) the data likelihood
+#' only ("conditional", default) or (b) the joint likelihood including the
+#' hierarchical prior over subject-level parameters ("joint", non-standard, experimental).
+#'
+#' If `use_best_fit = TRUE` (default), the deviance anchor is taken as the better
+#' of the deviance at the posterior mean parameters and the best-fitting posterior
+#' draw. If `FALSE`, the deviance at the posterior mean parameters is used
+#' (standard DIC/BPIC).
 #'
 #' @param sList List of samples objects
 #' @param stage A string. Specifies which stage the samples are to be taken from `"preburn"`, `"burn"`, `"adapt"`, or `"sample"`
 #' @param filter An integer or vector. If it's an integer, iterations up until the value set by `filter` will be excluded.
 #' If a vector is supplied, only the iterations in the vector will be considered.
-#' @param use_best_fit Boolean, defaults to `TRUE`, uses the minimal or mean likelihood (whichever is better) in the
-#' calculation, otherwise always uses the mean likelihood.
-#' @param BayesFactor Boolean, defaults to `TRUE`. Include marginal likelihoods as estimated using WARP-III bridge sampling.
+#' @param use_best_fit Boolean; defaults to `TRUE` If `TRUE`, uses the smaller of (i) the deviance at the posterior mean parameters and (ii) the lowest deviance across posterior draws (i.e., the best-fitting draw). If `FALSE`, uses only the deviance at the posterior mean parameters (i.e., standard DIC/BPIC).
+#' @param type Character. `"conditional"` (default) uses only the data likelihood
+#' for DIC/BPIC. `"joint"` uses the joint likelihood including the hierarchical
+#' prior; this option is experimental.
+#' @param BayesFactor Boolean, defaults to `TRUE`. Include marginal deviance
+#' (`-2 * log` marginal likelihood) as estimated using WARP-III bridge sampling.
 #' Usually takes a minute per model added to calculate
 #' @param cores_for_props Integer, how many cores to use for the Bayes factor calculation, here 4 is the default for the 4 different proposal densities to evaluate, only 1, 2 and 4 are sensible.
 #' @param cores_per_prop Integer, how many cores to use for the Bayes factor calculation if you have more than 4 cores available. Cores used will be cores_for_props * cores_per_prop. Best to prioritize cores_for_props being 4 or 2
@@ -45,8 +57,10 @@
 #' @export
 
 compare <- function(sList,stage="sample",filter=NULL,use_best_fit=TRUE,
+                        type = "conditional",
                         BayesFactor = TRUE, cores_for_props =4, cores_per_prop = 1,
                         print_summary=TRUE,digits=0,digits_p=3, ...) {
+  type <- match.arg(type, c("conditional","joint"))
   if(is(sList, "emc")) sList <- list(sList)
   getp <- function(IC) {
     IC <- -(IC - min(IC))/2
@@ -60,7 +74,7 @@ compare <- function(sList,stage="sample",filter=NULL,use_best_fit=TRUE,
   ICs <- setNames(vector(mode="list",length=length(sList)),names(sList))
   for (i in 1:length(ICs)) ICs[[i]] <- IC(sList[[i]],stage=stage,
                                           filter=sflist[[i]],use_best_fit=use_best_fit,subject=list(...)$subject,print_summary=FALSE,
-                                          group_only = dots$group_only)
+                                          group_only = dots$group_only, type = type)
   ICs <- data.frame(do.call(rbind,ICs))
   DICp <- getp(ICs$DIC)
   BPICp <- getp(ICs$BPIC)
@@ -250,14 +264,20 @@ gelman_diag_robust <- function(mcl,autoburnin = FALSE,transform = TRUE, omit_mps
 
 IC <- function(emc,stage="sample",filter=0,use_best_fit=TRUE,
                print_summary=TRUE,digits=0,subject=NULL,
-               group_only = FALSE)
+               group_only = FALSE, type = "conditional")
   # Gets DIC, BPIC, effective parameters, mean deviance, and deviance of mean
 {
+  type <- match.arg(type, c("conditional","joint"))
   # Mean log-likelihood for each subject
-  ll <- get_pars(emc, stage = stage, filter = filter, selection = "LL", merge_chains = TRUE)
+  if (length(subject)!=1) {
+    ll <- get_pars(emc, stage = stage, filter = filter, selection = "LL", merge_chains = TRUE)
+    alpha <- get_pars(emc,selection="alpha",stage=stage,filter=filter, by_subject = TRUE, merge_chains = TRUE)
+  } else {
+    ll <- get_pars(emc, stage = stage, filter = filter, selection = "LL", merge_chains = TRUE,subject=subject)
+    alpha <- get_pars(emc,selection="alpha",stage=stage,filter=filter, by_subject = TRUE, merge_chains = TRUE,subject=subject)
+  }
   minDs <- -2*apply(ll[[1]][[1]], 2, max)
   mean_lls <- apply(ll[[1]][[1]], 2, mean)
-  alpha <- get_pars(emc,selection="alpha",stage=stage,filter=filter, by_subject = TRUE, merge_chains = TRUE)
   mean_pars <- lapply(alpha,function(x){apply(do.call(rbind,x),2,mean)})
   # log-likelihood for each subject using their mean parameter vector
   data <- emc[[1]]$data
@@ -271,7 +291,7 @@ IC <- function(emc,stage="sample",filter=0,use_best_fit=TRUE,
     Dmeans <- Dmeans[subject[1]]
     mean_lls <- mean_lls[subject[1]]
     minDs <- minDs[subject[1]]
-  } else{
+  } else if (type == "joint"){
     group_stats <- group_IC(emc, stage=stage,filter=filter, type = emc[[1]]$type)
     if(group_only){
       mean_lls <- group_stats$mean_ll
@@ -282,6 +302,8 @@ IC <- function(emc,stage="sample",filter=0,use_best_fit=TRUE,
       minDs <- c(minDs, group_stats$minD)
       Dmeans <- c(Dmeans, group_stats$Dmean)
     }
+  } else if (group_only) {
+    warning("group_only ignored for conditional DIC; using data likelihood only.")
   }
   if (use_best_fit) minDs <- pmin(minDs,Dmeans)
 
@@ -338,8 +360,11 @@ get_BayesFactor <- function(MLL1, MLL2){
 #' If a vector is supplied, only the iterations in the vector will be considered.
 #' @param use_best_fit Boolean, defaults to `TRUE`, use minimal likelihood or mean likelihood
 #' (whichever is better) in the calculation, otherwise always uses the mean likelihood.
-#' @param print_summary Boolean (defaults to `TRUE`) print table of results
+#' @param print_summary Boolean (defaults to `TRUE`) print tables of model weight results
+#' @param return_summary Return tables of model weight results
 #' @param digits Integer, significant digits in printed table
+#' @param n_cores Number of cores for parallel processing
+#' @param subject Used to select subset of subjects (integer or character vector)
 #'
 #' @return List of matrices for each subject of effective number of parameters,
 #' mean deviance, deviance of mean, DIC, BPIC and associated weights.
@@ -350,21 +375,40 @@ get_BayesFactor <- function(MLL1, MLL2){
 #' compare_subject(list(m0 = samples_LNR, m1 = samples_LNR))
 #' @export
 compare_subject <- function(sList,stage="sample",filter=0,use_best_fit=TRUE,
-                            print_summary=TRUE,digits=3) {
+  print_summary=TRUE,digits=3,return_summary=FALSE,n_cores=1,subject=NULL) {
+
+  compare_one <- function(subject,sList,stage,filter,use_best_fit)
+    compare(sList,subject=subject,stage=stage,filter=filter,use_best_fit=use_best_fit,
+            BayesFactor=FALSE,print_summary=FALSE)
+
   if(is(sList, "emc")) sList <- list(sList)
   subjects <- names(sList[[1]][[1]]$data)
-  is_single <- sapply(sList, function(x) return(x[[1]]$type == "single"))
-  if(any(!is_single)) warning("subject-by-subject comparison is best done with models of type `single`")
-  out <- setNames(vector(mode="list",length=length(subjects)),subjects)
-  for (i in subjects) out[[i]] <- compare(sList,subject=i,BayesFactor=FALSE,
-                                          stage=stage,filter=filter,use_best_fit=use_best_fit,print_summary=FALSE)
-  if (print_summary) {
-    wDIC <- lapply(out,function(x)x["wDIC"])
-    wBPIC <- lapply(out,function(x)x["wBPIC"])
-    pDIC <- do.call(rbind,lapply(wDIC,function(x){
+  if (!is.null(subject)) if (is.integer(subject)) subjects <- subjects[subject] else
+    if (is.character(subject))
+      if (!all(subject %in% subjects)) stop("subject name(s) not in subjects") else
+      subjects <- subject
+  # is_single <- sapply(sList, function(x) return(x[[1]]$type == "single"))
+  # if(any(!is_single)) warning("subject-by-subject comparison is best done with models of type `single`")
+  if (n_cores>1) {
+    out <- auto_mclapply(subjects,compare_one,sList=sList,stage=stage,filter=filter,
+                   use_best_fit=use_best_fit,mc.cores=n_cores)
+    names(out) <- subjects
+  } else {
+    out <- setNames(vector(mode="list",length=length(subjects)),subjects)
+    for (i in subjects) {
+      cat(".")
+      out[[i]] <- compare(sList,subject=i,BayesFactor=FALSE,
+        stage=stage,filter=filter,use_best_fit=use_best_fit,print_summary=FALSE)
+     }
+  }
+  wDIC <- lapply(out,function(x)x["wDIC"])
+  wBPIC <- lapply(out,function(x)x["wBPIC"])
+  pDIC <- do.call(rbind,lapply(wDIC,function(x){
       setNames(data.frame(t(x)),paste("wDIC",rownames(x),sep="_"))}))
-    pBPIC <- do.call(rbind,lapply(wBPIC,function(x){
+  pBPIC <- do.call(rbind,lapply(wBPIC,function(x){
       setNames(data.frame(t(x)),paste("wBPIC",rownames(x),sep="_"))}))
+  if (print_summary) {
+    # WILL NEED FORMAT FIXES IF REINCLUDED
     # if (BayesFactor) {
     #   pMD <- do.call(rbind,lapply(wMD,function(x){
     #   setNames(data.frame(t(x)),paste("wMD",rownames(x),sep="_"))}))
@@ -376,13 +420,16 @@ compare_subject <- function(sList,stage="sample",filter=0,use_best_fit=TRUE,
     #               MD=table(mnams[apply(pMD,1,which.max)])))
     #
     # } else {
-    print(round(cbind(pDIC,pBPIC),digits))
-    mnams <- unlist(lapply(strsplit(dimnames(pDIC)[[2]],"_"),function(x){x[[2]]}))
+    print(list(DIC=round(pDIC,digits),BPIC=round(pBPIC,digits)))
+    mnams <- unlist(lapply(strsplit(dimnames(pDIC)[[2]],"_"),function(x){
+      if (length(x)==2) x[[2]] else paste(x[-1],collapse="_")
+    }))
     cat("\nWinners\n")
     print(rbind(DIC=table(mnams[apply(pDIC,1,which.max)]),
                 BPIC=table(mnams[apply(pBPIC,1,which.max)])))
     # }
   }
+  if (return_summary) out <- list(DIC=pDIC,BPIC=pBPIC)
   invisible(out)
 }
 
@@ -599,4 +646,3 @@ model_averaging <- function(IC_for, IC_against) {
     Factor = bayes_factor
   ))
 }
-
